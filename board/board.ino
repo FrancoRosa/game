72bf32b8adb40e1a31ea245199ceb255bb468d35
FrancoRosa <km115.franco@gmail.com>
2023-10-05 11:09:12 -0500

    Add car controller

--- board/board.ino
+++ board/board.ino
@@ -1,58 +1,124 @@
-
 #define led 13
-// shift register pinout
+
 #define enable 5
-#define data 2  // ds data (14)
-#define clock 4 //  (11) SHCP
-#define latch 3 //  (12) STCP
+#define data 2
+#define clock 4
+#define latch 3
 
 #define i0 A0
 #define i1 A1
 #define i2 A2
 
+#define button A3
+#define buzzer A4
+#define cards 4
+#define reeds 3
+
+int values[cards][reeds] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+bool running = false;
 bool a = false;
 int i = 0;
 int card[] = {
-    0b00000010,
-    0b00000100,
-    0b00001000,
-    0b00010000,
+    0b00000010, 0b00000100, 0b00001000, 0b00010000,
+
+    // 0b11111101,
+    // 0b11111011,
+    // 0b11110111,
+    // 0b11101111,
 };
+int clear = 0b00000000;
+// int clear = 0b11111111;
 
 int val = 0;
 
-int getval() {
-  Serial.print(analogRead(i2));
+bool getval(int i) {
+  // values[i][0] = digitalRead(i1);
+  // values[i][1] = digitalRead(i0);
+  // values[i][2] = digitalRead(i2);
+  // return values[i][0] > 500 || values[i][1] > 500 || values[i][2] > 500;
+
+  Serial.print(!digitalRead(i1));
+  Serial.print(!digitalRead(i0));
+  Serial.print(!digitalRead(i2));
+  Serial.println();
+}
+
+void print_reeds() {
+  for (int i = 0; i < cards; i++) {
+    print_reed(i);
+  }
+}
+
+void print_reed(int i) {
+  Serial.print("card ");
+  Serial.print(i);
+  Serial.print(": ");
+  Serial.print(values[i][0]);
   Serial.print(" ");
-  Serial.print(analogRead(i1));
+  Serial.print(values[i][1]);
   Serial.print(" ");
-  Serial.println(analogRead(i0));
+  Serial.print(values[i][2]);
+  Serial.println();
+}
+
+void beep() {
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(led, HIGH);
+  delay(50);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(led, LOW);
 }
 
 void setup() {
-  pinMode(led, OUTPUT);
   Serial.begin(9600);
   Serial.println("... start");
 
+  pinMode(led, OUTPUT);
   pinMode(latch, OUTPUT);
   pinMode(clock, OUTPUT);
   pinMode(data, OUTPUT);
 
   pinMode(enable, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+
+  pinMode(i0, INPUT_PULLUP);
+  pinMode(i1, INPUT_PULLUP);
+  pinMode(i2, INPUT_PULLUP);
 
   digitalWrite(enable, LOW);
+  digitalWrite(buzzer, LOW);
+  for (int i = 0; i < 5; i++) {
+    beep();
+    delay(100);
+  }
+}
+
+void clear_register() {
+  digitalWrite(latch, LOW);
+  shiftOut(data, clock, MSBFIRST, clear);
+  digitalWrite(latch, HIGH);
 }
 
 void loop() {
+  if (!digitalRead(button)) {
+    running = !running;
+    beep();
+    print_reeds();
+    delay(200);
+  }
 
   for (int i = 0; i < 4; i++) {
     digitalWrite(latch, LOW);
     shiftOut(data, clock, MSBFIRST, card[i]);
     digitalWrite(latch, HIGH);
-    Serial.print(i);
-    Serial.print(":    ");
-    delay(100);
-    getval();
-    delay(1000);
+
+    getval(i);
+    // print_reed(i);
+    delay(5000);
   }
+  // clear_register();
+  // print_reeds();
+  // delay(500);
 }
---
+++ car/car.ino
@@ -0,0 +1,125 @@
+#include <SoftwareSerial.h>
+#include <TimerOne.h>
+
+#define ledPin 13
+#define rxPin 10
+#define txPin 11
+
+#define mr1l 2
+#define mr1r 3
+#define ml1l 6
+#define ml1r 7
+
+int counter = 0;
+
+enum state_t { st_idle, st_bck, st_fwd, st_right, st_left };
+
+state_t state = st_idle;
+
+SoftwareSerial btSerial(rxPin, txPin);
+
+void fwr() {
+  digitalWrite(mr1l, HIGH);
+  digitalWrite(ml1l, HIGH);
+}
+
+void bck() {
+  digitalWrite(mr1r, HIGH);
+  digitalWrite(ml1r, HIGH);
+}
+
+void lft() {
+  digitalWrite(mr1l, HIGH);
+  digitalWrite(ml1r, HIGH);
+}
+
+void rgt() {
+  digitalWrite(mr1r, HIGH);
+  digitalWrite(ml1l, HIGH);
+}
+
+void off() {
+  digitalWrite(mr1l, LOW);
+  digitalWrite(mr1r, LOW);
+  digitalWrite(ml1l, LOW);
+  digitalWrite(ml1r, LOW);
+}
+
+void process_command() {
+  char c;
+  while (btSerial.available()) {
+    c = btSerial.read();
+    Serial.println(c);
+    counter = 0;
+    switch (c) {
+    case 'f':
+      if (state != st_fwd)
+        off();
+      state = st_fwd;
+      break;
+    case 'b':
+      if (state != st_bck)
+        off();
+      state = st_bck;
+      break;
+    case 'l':
+      if (state != st_left)
+        off();
+      state = st_left;
+      break;
+    case 'r':
+      if (state != st_right)
+        off();
+      state = st_right;
+      break;
+
+    default:
+      state = st_idle;
+      break;
+    }
+  }
+}
+
+void blink() {
+  digitalWrite(ledPin, !digitalRead(ledPin));
+  counter++;
+  if (counter > 4) {
+    counter = 0;
+    state = st_idle;
+  }
+  switch (state) {
+  case st_idle:
+    off();
+    break;
+  case st_fwd:
+    fwr();
+    break;
+  case st_bck:
+    bck();
+    break;
+  case st_left:
+    lft();
+    break;
+  case st_right:
+    rgt();
+    break;
+  default:
+    break;
+  }
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(mr1l, OUTPUT);
+  pinMode(mr1r, OUTPUT);
+  pinMode(ml1l, OUTPUT);
+  pinMode(ml1r, OUTPUT);
+  off();
+  Timer1.initialize(100000); // uS
+  Timer1.attachInterrupt(blink);
+  btSerial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("...Start");
+}
+
+void loop() { process_command(); }
